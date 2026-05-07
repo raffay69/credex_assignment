cc6e386b4e0dfc7dbd23fca0768e04bab64f084d
Mohd Abdul raffay <mohdabdulraffay@Mohds-MacBook-Air.local>
2026-05-08 02:09:37 +0530

    fix: remove unecessary toaster tag from Home.tsx

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,7 +1,6 @@
 import { useState } from 'react'
 import AuditResults from './Results'
 import AuditForm from './Form'
-import { Toaster } from 'react-hot-toast'
 
 function Home() {
   const [ state , setState] = useState("form")
@@ -9,7 +8,6 @@ function Home() {
 
   return (
     <>
-    <Toaster />
     {state === "form" ? <AuditForm setState={setState} setData={setData} /> : <AuditResults setState={setState} data={data}/> }
     </>
   )
