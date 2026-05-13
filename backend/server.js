58dde16d2a13769693934197a8ed8e39c173e579
Mohd Abdul raffay <mohdabdulraffay@Mohds-MacBook-Air.local>
2026-05-13 17:33:32 +0530

    feat: added a health endpoint in server

--- backend/server.js
+++ backend/server.js
@@ -22,7 +22,7 @@ const limiter = rateLimit({
 
 
 const app = express()
-const PORT = 3000
+const PORT =  process.env.PORT || 3000
 
 app.use(cors())
 app.use(limiter)
@@ -121,6 +121,10 @@ app.post("/email" , async (req , res)=>{
     }
 })
 
+
+app.get("/health" , (req , res)=>{
+    res.status(200).send("healthy")
+})
 
 
 app.listen(PORT , ()=>{
